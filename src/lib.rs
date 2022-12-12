5ecb73400f07576dfcf35d55894f5aae256ec8bf
chrysn <chrysn@fsfe.org>
2022-12-12 15:42:58 +0100

    Initial version

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+//! Implementation utilities for CoAP-over-GATT ([draft-amsuess-core-coap-over-gatt-02])
+//!
+//! Right now, this contains the message format parsing and serialization, largely fulled by
+//! [coap_message_utils], given that most of the message format is just a rehash of the universal
+//! CoAP option-extension-data-ff-payload scheme.
+//!
+//! [draft-amsuess-core-coap-over-gatt-02]: https://datatracker.ietf.org/doc/id/draft-amsuess-core-coap-over-gatt-02.html
+#![no_std]
+
+pub type ReadMessage<'a> = coap_message_utils::inmemory::Message<'a>;
+pub type WriteMessage<'a> = coap_message_utils::inmemory_write::Message<'a>;
+
+/// Error type for trying to parse a zero-length message
+#[derive(Debug)]
+pub struct MessageTooShort;
+
+/// Return a readable message version of data received over CoAP-over-GATT.
+///
+/// Note that, as explained in [coap_message_utils::inmemory::Message], a successful
+/// result does not mean that the message is valid as a whole -- that'd entail "expensive" parsing
+/// of the message ahead of time. Instead, if invalid properties are discovered at processing time,
+/// the message will pretend to contain an option that the application does not understand.
+// FIXME returning the concrete type is convenient as it allows a few types to be known, but may be
+// limiting on the long run
+pub fn parse(serialized: &[u8]) -> Result<ReadMessage<'_>, MessageTooShort> {
+    Ok(coap_message_utils::inmemory::Message::new(*serialized.get(0).ok_or(MessageTooShort)?, &serialized[1..]))
+}
+
+/// Dress up a writable buffer of a constant size as a writable CoAP message
+///
+/// ## Open issues
+///
+/// Can we do any better than sending the user through a closure? It's not like we have to do any
+/// actual cleanup, we just^TM need to create the inmemory_write::Message with a reference, and be
+/// there when it is destructed.
+///
+/// In an even advanced version, this might take an in-place writer to place the bytes into, rather
+/// than moving a [heapless::Vec] around (and hoping for return value optimization).
+// FIXME as for parse()
+pub fn write<const N: usize>(closure: impl FnOnce(&mut WriteMessage<'_>)) -> heapless::Vec<u8, N> {
+    let mut buffer = heapless::Vec::<u8, N>::new();
+    // FIXME can we get rid of these needless writes? Probably inmemory_write would need a
+    // better backend than just a slice
+    buffer.resize_default(N).unwrap();
+
+    let (codebuf, tailbuf) = buffer.split_at_mut(1);
+
+    let mut message = coap_message_utils::inmemory_write::Message::new(&mut codebuf[0], tailbuf);
+    closure(&mut message);
+    let len = message.finish();
+    buffer.truncate(len + 1);
+    buffer
+}
